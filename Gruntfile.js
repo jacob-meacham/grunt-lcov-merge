6329f4bc52fed1708dedcd068328c42bc5317c64
Jacob Meacham <jacob.e.meacham@gmail.com>
2015-01-28 12:26:58 -0800

    Prepare for travis.

--- Gruntfile.js
+++ Gruntfile.js
@@ -29,31 +29,28 @@ module.exports = function(grunt) {
     },
 
     lcovMerge: {
-      ci: {
-        options: {
-            emitters: ['event']
-        }
+      options: {
+          emitters: ['event']
       },
-      files: ['build/coverage/**/*.info']
+      files: ['build/coverage/*.info', 'build/coverage/**/*.info']
     }
   });
 
   grunt.event.on('coverage', function(lcov, done) {
-      console.log('Coverage event!');
-      done();
-      // require('coveralls').handleInput(lcov, function(err) {
-      //     if (err) {
-      //         return done(err);
-      //     }
-      //     done();
-      // });
+    console.log(lcov);
+    require('coveralls').handleInput(lcov, function(err) {
+        if (err) {
+          return done(err);
+        }
+        done();
+    });
   });
 
   grunt.loadTasks('tasks');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-mocha-istanbul');
 
-  grunt.registerTask('ci', ['jshint', 'mocha_istanbul', 'lcovMerge:ci']);
+  grunt.registerTask('ci', ['jshint', 'mocha_istanbul', 'lcovMerge']);
   grunt.registerTask('default', ['jshint', 'mocha_istanbul']);
 
 };
